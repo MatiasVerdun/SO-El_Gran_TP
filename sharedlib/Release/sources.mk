074bfff2e02ae2181339763215acaebd0aba17a3
MatiasVerdun <matiasverdun@hotmail.com>
2018-12-03 20:43:17 -0300

    Posible makefile

---
+++ sharedlib/Release/sources.mk
@@ -0,0 +1,23 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+OBJ_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+O_SRCS := 
+S_UPPER_SRCS := 
+LIBRARIES := 
+OBJS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+archivos \
+commons \
+commons/collections \
+conexiones \
+console \
+dtbSerializacion \
+parser \
+
