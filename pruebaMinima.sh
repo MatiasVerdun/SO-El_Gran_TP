157f5ae59bf2539a79ec90ad2c8f44cdca4cb7ad
delucamariano <deluca.mariano96@gmail.com>
2018-12-05 17:38:37 -0300

    Update pruebaMinima.sh (Mati es un forro)

--- pruebaMinima.sh
+++ pruebaMinima.sh
@@ -1,12 +1,12 @@
 bash clean.sh
 cd Config/Pruebas
-cp CPUMinima ../CPU.txt
+cp CPUMinima.txt ../CPU.txt
 cd Pruebas
-cp S-AFAMinima ../S-AFA.txt
+cp S-AFAMinima.txt ../S-AFA.txt
 cd Pruebas
-cp DAMMinima ../DAM.txt
+cp DAMMinima.txt ../DAM.txt
 cd Pruebas
-cp MDJMinima ../MDJ.txt
+cp MDJMinima.txt ../MDJ.txt
 cd Pruebas
-cp FM9Minima ../FM9.txt
+cp FM9Minima.txt ../FM9.txt
 cd ../..
