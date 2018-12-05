1c42d0cd0df96ef8e817e70e391043615b018946
MatiasVerdun <matiasverdun@hotmail.com>
2018-12-05 17:42:35 -0300

    Update pruebaFile.sh

--- pruebaFile.sh
+++ pruebaFile.sh
@@ -1,12 +1,8 @@
 bash clean.sh
 cd Config/Pruebas
 cp CPUFile.txt ../CPU.txt
-cd Pruebas
 cp S-AFAFile.txt ../S-AFA.txt
-cd Pruebas
 cp DAMFile.txt ../DAM.txt
-cd Pruebas
 cp MDJFile.txt ../MDJ.txt
-cd Pruebas
 cp FM9File.txt ../FM9.txt
 cd ../..
