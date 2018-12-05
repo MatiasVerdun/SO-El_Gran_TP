4127a09a7d7c0a70a80ca1da4bf4934603b5206e
delucamariano <deluca.mariano96@gmail.com>
2018-12-05 17:42:58 -0300

    Update pruebaAlgoritmos.sh

--- pruebaAlgoritmos.sh
+++ pruebaAlgoritmos.sh
@@ -1,12 +1,8 @@
 bash clean.sh
 cd Config/Pruebas
 cp CPUAlgoritmos.txt ../CPU.txt
-cd Pruebas
 cp S-AFAAlgoritmos.txt ../S-AFA.txt
-cd Pruebas
 cp DAMAlgoritmos.txt ../DAM.txt
-cd Pruebas
 cp MDJAlgoritmos.txt ../MDJ.txt
-cd Pruebas
 cp FM9Algoritmos.txt ../FM9.txt
 cd ../..
