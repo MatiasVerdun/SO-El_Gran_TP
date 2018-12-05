99eb2d111656d055c33384ea25301f09dac266c2
MatiasVerdun <matiasverdun@hotmail.com>
2018-12-05 17:42:06 -0300

    Update pruebaMinima.sh

--- pruebaMinima.sh
+++ pruebaMinima.sh
@@ -1,12 +1,8 @@
 bash clean.sh
 cd Config/Pruebas
 cp CPUMinima.txt ../CPU.txt
-cd Pruebas
 cp S-AFAMinima.txt ../S-AFA.txt
-cd Pruebas
 cp DAMMinima.txt ../DAM.txt
-cd Pruebas
 cp MDJMinima.txt ../MDJ.txt
-cd Pruebas
 cp FM9Minima.txt ../FM9.txt
 cd ../..
