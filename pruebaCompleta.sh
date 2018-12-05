cfcf95907a00502ae9d459be45990f2d2c2a03e1
delucamariano <deluca.mariano96@gmail.com>
2018-12-05 17:42:40 -0300

    Update pruebaCompleta.sh

--- pruebaCompleta.sh
+++ pruebaCompleta.sh
@@ -1,12 +1,8 @@
 bash clean.sh
 cd Config/Pruebas
 cp CPUCompleta.txt ../CPU.txt
-cd Pruebas
 cp S-AFACompleta.txt ../S-AFA.txt
-cd Pruebas
 cp DAMCompleta.txt ../DAM.txt
-cd Pruebas
 cp MDJCompleta.txt ../MDJ.txt
-cd Pruebas
 cp FM9Completa.txt ../FM9.txt
 cd ../..
