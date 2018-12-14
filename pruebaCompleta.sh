0abb28d5261643f9d35e4e5a9f6023f62184c5aa
MatiasVerdun <matiasverdun@hotmail.com>
2018-12-14 11:46:40 -0300

    Agregando el reset del fifa para la prueba Completa

--- pruebaCompleta.sh
+++ pruebaCompleta.sh
@@ -5,4 +5,9 @@ cp S-AFACompleta.txt ../S-AFA.txt
 cp DAMCompleta.txt ../DAM.txt
 cp MDJCompleta.txt ../MDJ.txt
 cp FM9Completa.txt ../FM9.txt
-cd ../..
+cd ../../..
+cd fifa-examples
+git reset --hard
+git clean -fd
+cd ..
+cd tp-2018-2c-smlc
